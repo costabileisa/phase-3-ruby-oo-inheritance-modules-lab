132ffa90783fff514b97b98be4e4683c497bcf5c
Isabella Costabile <costabileisa@gmail.com>
2022-12-31 21:32:48 -0500

    commit tree 3763a943dae990797942821e47c6a5fe49766e1e

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,14 @@
 require 'pry'
+require_relative "./concerns/memorable"
+require_relative "./concerns/find_by_name"
+require_relative "./concerns/paramable"
 
 class Artist
-  attr_accessor :name
-  attr_reader :songs
+  extend Memorable
+  extend Findable
+  include Paramable
 
+  attr_reader :songs
   @@artists = []
 
   def initialize
@@ -11,32 +16,15 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
